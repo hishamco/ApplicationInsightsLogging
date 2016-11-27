a15fbd8062e4032b75b05732dcc7eb87ca2b0ca6
hishamco <hishamco_2007@yahoo.com>
2016-11-27 07:50:09 +0300

    Add implementation APIs for Application Insights logging

---
+++ src/ApplicationInsightsLogging/ApplicationInsightsLoggerFactoryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace ApplicationInsightsLogging
+{
+    public static class ApplicationInsightsLoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddApplicationInsights(this ILoggerFactory factory)
+        {
+            return AddApplicationInsights(factory, new ApplicationInsightsSettings());
+        }
+
+        public static ILoggerFactory AddApplicationInsights(
+            this ILoggerFactory factory,
+            Func<string, LogLevel, bool> filter)
+        {
+            factory.AddProvider(new ApplicationInsightsLoggerProvider(filter));
+            return factory;
+        }
+
+        public static ILoggerFactory AddApplicationInsights(
+            this ILoggerFactory factory,
+            ApplicationInsightsSettings settings)
+        {
+            factory.AddProvider(new ApplicationInsightsLoggerProvider(settings));
+
+            return factory;
+        }
+    }
+}
